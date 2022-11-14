cbe862ed7859eda27024692c7e7fd4c951ddd1e2
Anna <ankakras@yandex.ru>
2022-11-14 14:48:55 +0700

    Решение

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -1,14 +1,4 @@
 ﻿// Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника
-/*void PrintMatrix(int[,] matrix)
-{
-    for(int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for(int j = 0; j < matrix.GetLength(1); j++)
-            if(matrix[i, j] != 0)
-                Console.Write($"{matrix[i, j], -5} ");
-        Console.WriteLine();
-    }
-}*/
 void Pascal(int[,] matrix)
 {
     for(int i = 0; i < matrix.GetLength(0); i++)
@@ -19,34 +9,29 @@ void Pascal(int[,] matrix)
     for(int i = 2; i < matrix.GetLength(0); i++)
         for(int j = 1; j <= i; j++)
             matrix[i, j] = matrix[i - 1, j - 1] + matrix[i - 1, j];
-    
-    //for(int i = 1; i < matrix.GetLength(0); i++)
-    //{
-      //  for(int j = n + 1; j < matrix.GetLength(1); j++)
-        //{
-          //  matrix[i, j] = matrix[i - 1, j - 1] + matrix[i - 1, j + 1];
-        //}
-    //}
+
 }
-//Console.WriteLine("Введите количество строк в треугольнике Паскаля: ");
-//int rows = int.Parse(Console.ReadLine() ?? "0");
-int rows = 8;
+void PrintTrianglePascal(int[,] matrix)
+{
+    int cellWidth = 3;
+    int rows = matrix.GetLength(0);
+    int col = cellWidth * rows;
+    for(int i = 0; i < rows; i ++)
+    {
+        for(int j = 0; j <= i; j++)
+        {
+            Console.SetCursorPosition(col, i + 4);
+            if(matrix[i, j] != 0)
+                Console.Write($"{matrix[i, j]}");
+            col += cellWidth * 2;
+        }
+        col = cellWidth * rows - cellWidth * (i + 1);
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите количество строк в треугольнике Паскаля: ");
+int rows = int.Parse(Console.ReadLine() ?? "0");
 int[,] matrix = new int[rows, rows];
 Pascal(matrix);
 Console.WriteLine("Треугольник Паскаля: ");
-//PrintMatrix(matrix);
-int cellWidth = 3;
-int col = cellWidth * rows;
-for(int i = 0; i < rows; i ++)
-{
-    for(int j = 0; j <= i; j++)
-    {
-        Console.SetCursorPosition(col, i + 1);
-        if(matrix[i, j] != 0)
-            Console.Write($"{matrix[i, j], -2}");
-        col += cellWidth * 2;
-    }
-    col = cellWidth * rows - cellWidth * (i + 1);
-    Console.WriteLine();
-}
-
+PrintTrianglePascal(matrix);
